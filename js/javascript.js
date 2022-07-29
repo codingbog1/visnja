4566263ee7b969872426e6593300581ef81b0111
codingbog1 <87824310+codingbog1@users.noreply.github.com>
2022-07-29 18:02:09 +0200

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,27 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-  document.querySelector('.submit-email').addEventListener('mousedown', (e) => {
-    e.preventDefault();
-    document.querySelector('.subscription').classList.add('done');
-  });
+}
+document.querySelector('.submit-email').addEventListener('mousedown', (e) => {
+  e.preventDefault();
+  document.querySelector('.subscription').classList.add('done');
+});
+
+function sendEmail() {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "bogdan.denic5@gmail.com",
+    Password: "bogdan123",
+    To: 'visnjadenic76@gmail.com',
+    From: document.getElementById("Email").value,
+    Subject: "Novi Email za newsletter",
+    Body: "Email:" + document.getElementById("Email").value
+  }).then(
+    message => alert("Email poslat")
+  );
+}
+
